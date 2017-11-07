1b1db11c593dd1239af3917c9e0ebc37a1e9938c
Andre <andrefelippe.weber@gmail.com>
2017-11-07 16:10:34 -0200

    add manager.h

--- manager.c
+++ manager.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>    
+#include "manager.h"
+
+int fsm_manager(int estado){
+
+	switch(estado){
+		case Esperando_configuracao:
+			break;
+		case aguardando_status:
+			break;
+		case mostrando_status:
+			break;
+	}
+	return 0;
+}
+
 int main() {
-    char byte;
+ /*   char byte;
     int fd = open("/dev/ttyUSB1", O_RDWR);
     write(fd, "teste", 5);
     //ssize_t size = read(fd, &byte, 1);
     //printf("Read byte %c\n", byte);
+*/
+
+   print
+
+
     return 0;
 }
---
+++ manager.h
@@ -0,0 +1,7 @@
+#ifndef MANAGER
+#define MANAGER
+
+enum estados{Esperando_configuracao, aguardando_status, mostrando_status};
+int estado = Esperando_configuracao;
+
+#endif
