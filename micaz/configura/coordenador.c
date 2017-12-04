7810f4474bbb520455186b10adccdeb4d85ac86b
AndreFWeber <andrefelippe.weber@gmail.com>
2017-12-04 11:54:28 +0100

    coordenador inicializa um experimento e recebe quantidade de pacotes enviados e recebidos

--- micaz/configura/coordenador.c
+++ micaz/configura/coordenador.c
@@ -16,28 +16,26 @@
 #include "node-id.h"
 #include <stdlib.h>
 
+
 #define DEBUG DEBUG_PRINT
 #include "net/ip/uip-debug.h"
-
-#define NUM_MAX_MOTES 9
 
 /*---------------------------------------------------------------------------*/
 PROCESS(coordenador_process, "Coordenador");
 AUTOSTART_PROCESSES(&coordenador_process);
 
 /*---------------------------------------------------------------------------*/
-  int dados_unicast=0;
+int dados_unicast=0;
 int motes[NUM_MAX_MOTES]={0};
 static void
 recv_uc(struct unicast_conn *c, const linkaddr_t *from)
 {
-  //printf("unicast message received from %d.%d\n", from->u8[0], from->u8[1]); 
+  printf("Unicast: %d diz %s\n", from->u8[0], (char *)packetbuf_dataptr());
   int i;
   for(i=0; i<NUM_MAX_MOTES; i++){
 	if(motes[i]==from->u8[0]) return;
   }
   
-  //Assumo que só me responde quem ainda não está na lista!	
   motes[dados_unicast++]=from->u8[0];
 }
 static const struct unicast_callbacks unicast_callbacks = {recv_uc};
@@ -60,27 +58,35 @@ rx_input_byte(unsigned char c){
 		recvd[--pos]='\0'; 
 		FSMCoordinator();		
 	}
-	leds_toggle(LEDS_GREEN);
 	return 1; 
 }
 
-int broadcast_status=0;
+//Liga o timer na thread 
+int espera_resposta=0;
 
 void sendBroadcast(char* msg){
-    //char msg[] = "broadcast_status";
-    packetbuf_copyfrom(msg, sizeof(msg));
+    printf("Enviando (sendBroadcast) %s\n", msg);
+    packetbuf_copyfrom(msg, strlen(msg)+1);
     broadcast_send(&broadcast);
-    broadcast_status=1;
-   // printf("broadcast\n");
+    espera_resposta=1;
 }
 
+void sendUnicast(linkaddr_t addr, char* msg){
+    printf("Enviando (sendUnicast) %s\n", msg);
+    packetbuf_copyfrom(msg, strlen(msg)+1);
+    if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
+      unicast_send(&uc, &addr);
+      espera_resposta=1;
+    }
+}
+
+int printI=0;			
 void FSMCoordinator(){
 	switch (estado){
 		case Aguardando_cmd:
 			if(strcmp("hello_coordinator", recvd)==0){
 				//!ok
 				printf("ok\n");
-				leds_toggle(LEDS_RED);
 			}
 			else
 			if(strcmp("leStatus_motes", recvd)==0){
@@ -90,7 +96,15 @@ void FSMCoordinator(){
 			else
 			if(strcmp("configura", recvd)==0){
 				estado=Configurando;
-				printf("ok\n");	
+				printf("ok\n");
+			}
+			if(strcmp("start", recvd)==0){
+				estado=Aguardando_confirmacao_conf;
+				sendBroadcast("start");
+			}
+			if(strcmp("stop", recvd)==0){
+				estado=medicao;
+				sendBroadcast("stop");
 			}
 			break;
 
@@ -99,24 +113,32 @@ void FSMCoordinator(){
 			sendBroadcast("broadcast_status");
 			estado = Aguardando_Motes_Fase1;
 			break;
-		case Aguardando_Motes_Fase1:
-			printf("status_mote %d,%d,%d,%d,%d,%d,%d,%d,%d\n", motes[0], motes[1], motes[2], motes[3], motes[4], motes[5], motes[6], motes[7], motes[8] );
-
+		case Aguardando_Motes_Fase1:;
+			printf("status_mote:");
+			while(printI<dados_unicast)			
+				printf("%d,", motes[printI++]);
+			printf("\n");
+			printI=0;			
 			//!Broadcast_status 
 			sendBroadcast("broadcast_status");
 			estado = Aguardando_Motes_Fase2;
 			break;
-		case Aguardando_Motes_Fase2:
-			printf("status_mote %d,%d,%d,%d,%d,%d,%d,%d,%d\n", motes[0], motes[1], motes[2], motes[3], motes[4], motes[5], motes[6], motes[7], motes[8] );
+		case Aguardando_Motes_Fase2:;
+			printf("status_mote:");
+			while(printI<dados_unicast)			
+				printf("%d,", motes[printI++]);
+			printf("\n");
+			printI=0;			
 			printf("fim\n");
 			estado = Aguardando_cmd;
 			break;
 
 		case Configurando: ;
+			//sendBroadcast("configura");
 			//Divisoes da string
-			char* parametro =  malloc(sizeof(char));
-			char* ID  =  malloc(sizeof(char));
-			char* valor  =  malloc(sizeof(char));
+			char* parametro = malloc(MAX_SIZE_XML_PARAM);
+			char* ID  =  malloc(MAX_SIZE_XML_ID);
+			char* valor =  malloc(MAX_SIZE_XML_VALOR);
 
 			char* token;
 			char* string;
@@ -125,25 +147,50 @@ void FSMCoordinator(){
 
 			while ((token = strsep(&string, "/")) != NULL)
 			{
-			  if(split==0)
-			 	memcpy(parametro, token, sizeof *token);
-			    else
-			    if(split==1)
-			   	memcpy(ID, token, sizeof *token);
-			    else
-			    if(split==2)
-			   	memcpy(valor, token, sizeof *token);
-			    split++;
+			  if(split==0){
+			 	memcpy(parametro, token, strlen(token)+1);
+			  }else{
+			  	if(split==1){
+			   		memcpy(ID, token, strlen(token)+1);
+			 	}else{
+			  		if(split==2){
+			   			memcpy(valor, token, strlen(token)+1);
+					}
+				}
+			  }
+				split++;
 			}
-	
+
+			strcat(parametro,"/");
+			strcat(parametro,valor);
+
 			if(strcmp("0", ID)==0){//Envia as configurações por broadcast e espera
-				printf("Broadcast");
+				sendBroadcast("configura");
+				sendBroadcast(parametro);
+			}else{
+				linkaddr_t addr;
+				addr.u8[0] = atoi(ID);
+				addr.u8[1] = 0;
+				sendUnicast(addr, "configura");
+				sendUnicast(addr, parametro);
 			}
 
 			free(parametro);
 			free(ID);
 			free(valor);
 
+			estado = Aguardando_cmd;
+			break;
+		case Aguardando_confirmacao_conf:;
+			printf("startados ");
+			while(printI<dados_unicast)			
+				printf("%d,", motes[printI++]);
+			printf("\n");
+			printI=0;			
+			estado = Aguardando_cmd;
+			break;
+		case medicao:;
+			estado = Aguardando_cmd;
 			break;
 	}
 
@@ -179,16 +226,13 @@ PROCESS_THREAD(coordenador_process, ev, data)
 	  etimer_set(&et, CLOCK_SECOND/50); //----------------------REVER ISSO! PQ SÓ FUNCIONA SE TEM O TIMER E O PROCESS_WAIT...
 	  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-	  if(broadcast_status==1){
-		  broadcast_status=0;
-		 // printf("Esperando timeout!\n");
-		 // if(dados_unicast==0) {
+	  if(espera_resposta==1){
+		  	  espera_resposta=0;
 			  etimer_set(&et, CLOCK_SECOND * 2);
 			  PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 			  //Esperou por 2 segundos uma resposta...Quem não respondeu vai ter que responder de novo na fase 2
 			 // printf("Timeout de espera do unicast!\n");
 			  FSMCoordinator();
-		  //}
 	  }
 
   }
--- micaz/configura/mote.c
+++ micaz/configura/mote.c
@@ -13,86 +13,246 @@
 #include "net/rime/collect.h"
 #include <stdio.h>
 #include "node-id.h"
+#include <stdlib.h>
+#include "mote.h"
+#include "net/mac/csma_variable.h"
+#include "cc2420.h"
+
 /*---------------------------------------------------------------------------*/
-PROCESS(broadcast_process, "Broadcast");
 PROCESS(experiment_process, "Experimento");
+PROCESS(sink_process, "sink");
+PROCESS(source_process, "source");
+AUTOSTART_PROCESSES(&experiment_process);
 
-AUTOSTART_PROCESSES(&broadcast_process, &experiment_process);
+static process_event_t PROCESS_EVENT_SINK;
 
 /*---------------------------------------------------------------------------*/
+
+void 
+configura(char *cfg){
+	//Divisoes da string
+	char* parametro = malloc(MAX_SIZE_XML_PARAM);
+	char* valor =  malloc(MAX_SIZE_XML_VALOR);
+
+	char* token;
+	char* string;
+	string = strdup(cfg);
+	uint8_t split=0;
+
+	while ((token = strsep(&string, "/")) != NULL)
+	{
+		if(split==0){
+		 	memcpy(parametro, token, strlen(token)+1);
+		}else{
+		   	memcpy(valor, token, strlen(token)+1);
+		}
+		split++;
+	}
+
+	if(strcmp("lambda", parametro)==0){	
+		lambda=atoi(valor);
+	}
+	if(strcmp("tamanho_pacote", parametro)==0){		
+		tamanhoPacote=atoi(valor);
+	}
+	if(strcmp("modo", parametro)==0){
+		if(strcmp("sink", valor)==0){	
+			sink=1;
+		}else{
+			sink=0;
+		}
+	}
+	if(strcmp("CSMA_MAX_BE", parametro)==0){		
+		set_mac_csma(CSMA_MAX_BE_, atoi(valor));
+	}
+	if(strcmp("CSMA_MIN_BE", parametro)==0){		
+		set_mac_csma(CSMA_MIN_BE_, atoi(valor));
+	}
+	if(strcmp("macMaxCSMABackoffs", parametro)==0){		
+		set_mac_csma(CSMA_MAX_BACKOFF_, atoi(valor));
+	}
+	if(strcmp("macMaxFramesRetries", parametro)==0){	 	
+		set_mac_csma(CSMA_MAX_MAX_FRAME_RETRIES_, atoi(valor));
+	}
+
+}
+
+void send_msg(linkaddr_t addr, char* msg){
+    packetbuf_copyfrom(msg, strlen(msg)+1);
+    if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
+      unicast_send(&uc, &addr);
+    }
+}
+/*---------------------------------------------------------------------------*/
+
+
 static void
 broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
 {
-  leds_toggle(LEDS_BLUE);//AMARELO!!!!
-  printf("broadcast message received from %d.%d: '%s'\n", from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
+  printf("chegou %s do %d por broadcast\n", (char *)packetbuf_dataptr(), from->u8[0]);
+  
+ if(from->u8[0]!=1){ //O coordenador da rede tem ID 1...
+	if(start==1 && running==1){
+//		estatisticas_sink(from->u8[0], (char *)packetbuf_dataptr());
+		if(sink==1){
+			process_post_synch(&sink_process, PROCESS_EVENT_SINK, &from->u8[0]);
+		}	
+	}
+  }else{//...Se recebeu do ID 1, então veio do coordenador
+	  if(strcmp((char *)packetbuf_dataptr(), "start")==0){
+		send_msg(*from, "ok");
+		start=1;
+	  }
+	  if(strcmp((char *)packetbuf_dataptr(), "stop")==0){
+		//send_msg(*from, "ok");
+		start=0;
+	  }
+	  if(strcmp((char *)packetbuf_dataptr(), "broadcast_status")==0)
+		send_msg(*from, "ok");
 
-  printf("responde broadcas\n");
-  send_msg(*from);
-
+	  if(configuraBool==1){
+		configura((char *)packetbuf_dataptr());
+		send_msg(*from, "ok");
+		configuraBool=0;
+	  }
+	  if(strcmp((char *)packetbuf_dataptr(), "configura")==0){
+		configuraBool=1;	
+	  }
+  }
 }
 static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
-static struct broadcast_conn broadcast;
 
 /*---------------------------------------------------------------------------*/
-
+static void
 recv_uc(struct unicast_conn *c, const linkaddr_t *from)
 {
-  printf("unicast message received from %d.%d\n", from->u8[0], from->u8[1]);
-  send_msg(*from);
-}
-static const struct unicast_callbacks unicast_callbacks = {recv_uc};
-static struct unicast_conn uc;
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(broadcast_process, ev, data)
-{ 
-  static struct etimer et;
-  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
-  PROCESS_BEGIN();
-
-  broadcast_open(&broadcast, 129, &broadcast_call);
-
-  while(1){
-    etimer_set(&et, CLOCK_SECOND * 2 + random_rand() % (CLOCK_SECOND * 4));
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-  } 
-
-  PROCESS_END();
-}
-/*---------------------------------------------------------------------------*/
-
-void send_msg(linkaddr_t addr){
-    packetbuf_copyfrom("Hello", 5);
-    if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
-      unicast_send(&uc, &addr);
-      leds_toggle(LEDS_GREEN);
-    }
-}
-
-PROCESS_THREAD(experiment_process, ev, data)
-{
-  PROCESS_BEGIN();
-
-  //NECESSÁRIO PARA PROGRAMAR O MICAZ
-  node_id_burn(3);
-
-  unicast_open(&uc, 146, &unicast_callbacks);
-
-  while(1) {
-    static struct etimer et;
-    linkaddr_t addr;
-    
-    etimer_set(&et, 5*CLOCK_SECOND);
-    
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
-
-
-    addr.u8[0] = 1;
-    addr.u8[1] = 0;
-
- //   send_msg(addr);
+  if(from->u8[0]!=1){ //O coordenador da rede tem ID 1...
+	if(start==1 && running==1){
+//		estatisticas_sink(from->u8[0], (char *)packetbuf_dataptr());
+		if(sink==1){
+			process_post_synch(&sink_process, PROCESS_EVENT_SINK, &from->u8[0]);
+		}	
+	}
+  }else{
+	  if(configuraBool==1){
+		configura((char *)packetbuf_dataptr());
+		send_msg(*from, "ok");
+		configuraBool=0;
+	  }
+	  if(strcmp((char *)packetbuf_dataptr(), "configura")==0){
+		configuraBool=1;	
+	  }
 
   }
+}
+static const struct unicast_callbacks unicast_callbacks = {recv_uc};
 
+/*--------------------------SINK------------------------------------*/
+
+PROCESS_THREAD(sink_process, ev, data)
+{
+
+  PROCESS_BEGIN();
+	  while(1) {
+	    PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_SINK);
+	    Sink_totalPacotesRecebidos++;
+	    Sink_pacoteID[*(int*)data]++;
+	  }
+  PROCESS_END();
+}
+
+/*--------------------------SOURCE------------------------------------*/
+PROCESS_THREAD(source_process, ev, data)
+{
+  PROCESS_BEGIN();
+	  while(1) {
+		    static struct etimer et;
+		    linkaddr_t addr;
+		    
+		    etimer_set(&et, 2*CLOCK_SECOND);
+		    
+		    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+		    packetbuf_copyfrom("Hello", 5);
+		    addr.u8[0] = 100;
+		    addr.u8[1] = 0;
+		    if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
+		      unicast_send(&uc, &addr);
+			Source_totalPacotesEnviados++;
+		    }
+	    }
+  PROCESS_END();
+}
+
+/*--------------------------MAIN------------------------------------*/
+char cfg[20];
+PROCESS_THREAD(experiment_process, ev, data)
+{
+//  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
+  PROCESS_BEGIN();
+
+  //Evento especifico para o sink desta plataforma
+  PROCESS_EVENT_SINK = process_alloc_event();
+
+  //NECESSÁRIO PARA PROGRAMAR O MICAZ
+  node_id_burn(100);
+  unicast_open(&uc, 146, &unicast_callbacks);
+  broadcast_open(&broadcast, 129, &broadcast_call);
+
+  linkaddr_t coordenador;
+  coordenador.u8[0] = 1;
+  coordenador.u8[1] = 0;
+
+  static struct etimer et;
+
+  while(1) {
+    etimer_set(&et, CLOCK_SECOND);
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+
+    if(start==1 && running==0){//Se recebeu um start
+	running=1;
+
+	if(sink==1){//Este mote será sink!
+		process_start(&sink_process, NULL);
+     		leds_toggle(LEDS_RED);
+        }else{
+		process_start(&source_process, NULL);		
+	        leds_toggle(LEDS_GREEN);
+	}
+    }
+    if(start==0 && running==1){
+	//Quando receber o stop
+	//pede para a thread repassar as estatisticas e então envia o sinal de exit.
+	if(sink==1){//Este mote será sink!
+		process_exit(&sink_process);
+		linkaddr_t addr;
+		sprintf(cfg, "TotalPacotes/%d", Sink_totalPacotesRecebidos);
+		packetbuf_copyfrom(&cfg, 20);
+		addr.u8[0] = 1;
+		addr.u8[1] = 0;
+		if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
+			unicast_send(&uc, &addr);
+		}
+	}else{
+		process_exit(&source_process);		
+		linkaddr_t addr;
+		sprintf(cfg, "TotalPacotes/%d", Source_totalPacotesEnviados);
+		packetbuf_copyfrom(&cfg, 20);
+		addr.u8[0] = 1;
+		addr.u8[1] = 0;
+		if(!linkaddr_cmp(&addr, &linkaddr_node_addr)) {
+			unicast_send(&uc, &addr);
+		}
+	}
+	running=0;
+     
+        //printf("valores %d e %d", Sink_totalPacotesRecebidos, Sink_pacoteID[15]);
+	leds_on(LEDS_ALL);
+
+
+    }
+
+  }
 
   PROCESS_END();
 
